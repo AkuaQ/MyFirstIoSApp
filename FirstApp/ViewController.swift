7f2ff4a8e20f385bbdc9646fd759b88312cd9fee
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-02-06 11:07:19 +0200

    Putting the same files in develop
    For consistency sake

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Akua Afrane-Okese on 2020/02/06.
+//  Copyright © 2020 Akua Afrane-Okese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
