8c706fe9f55e789d45fb7374733e6aa9f52ad3ad
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-02-07 08:44:09 +0200

    Create Flash button animation
    For each of the buttons, a flash animation was created. A few other animations are listed in the new UIButtonAnimation file.

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstButton: UIButton!
+    
+    
+    @IBOutlet weak var secondButton: UIButton!
+    
+    @IBOutlet weak var thirdButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func firstButtonTapped(_ sender: UIButton) {
+        sender.flash()
+        print("Facebook sign in chosen")
+    }
+    
+    @IBAction func secondButtonTapped(_ sender: UIButton) {
+        sender.flash()
+        print("Google sign in chosen")    }
+    
+    @IBAction func thirdButtonTapped(_ sender: UIButton) {
+        sender.flash()
+        print("Email sign in chosen")    }
+    
 }
 
