4faefdfb061dd130e16de3da231a5d50347436e5
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-02-20 09:00:16 +0200

    Cleaned Code and Loaded Images
    The reviewers' comments were taken into consideration and then action. 
    
    Images are now loaded into the table view when recipes are requested.

--- FirstApp/Constants.swift
+++ FirstApp/Constants.swift
@@ -2,8 +2,10 @@ import Foundation
 
 struct Constants {
     struct Storyboard {
-        static let homeViewController = "HomeVC"
+        static let homeViewController = "HomeViewController"
+        static  let recipeDetailViewController = "RecipeDetailViewController"
     }
+
     static func isPasswordValid(_ password: String) -> Bool {
         let passwordTest = NSPredicate(format: "SELF MATCHES %@",
                                     "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
