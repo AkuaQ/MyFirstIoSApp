f0587f9ffd4d8200018fe7a3880aa44e9ae319e1
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-03-31 07:37:44 +0200

    Changed structure of code

--- FirstAppTests/FirstAppTests-Bridging-Header.h
+++ FirstAppTests/FirstAppTests-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "*/Recipe/FavouritesObjectiveCUnitTests.m"
-#import "*/Recipe/FavouritesViewModelObjectiveCUnitTests.m"
+#import "*/Recipe/Model/FavouritesObjectiveCUnitTests.m"
+#import "*/Recipe/ViewModel/FavouritesViewModelObjectiveCUnitTests.m"
