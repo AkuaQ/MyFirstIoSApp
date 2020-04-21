478b0b51fd51f3a932dba18a9aa91c95bb9d38ed
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-04-21 06:51:26 +0200

    Changes to UI
    - Changes to UI in whole of app
    - Colour Sets created
    - Heroku server connection

--- FirstApp/Login/View/ViewController.swift
+++ FirstApp/Login/View/ViewController.swift
@@ -8,15 +8,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpElements()
+       let gradientLayer = CAGradientLayer()
+           var updatedFrame = self.navigationController!.navigationBar.bounds
+           updatedFrame.size.height += self.navigationController!.navigationBar.frame.height
+           gradientLayer.frame = updatedFrame
+           gradientLayer.colors = [UIColor(red: 75/255,
+                                           green: 29/255,
+                                           blue: 87/255, alpha: 1).cgColor, UIColor(red: 152/255,
+                                                                                    green: 103/255,
+                                                                                    blue: 163/255, alpha: 1).cgColor]
+           gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0) // vertical gradient start
+           gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0) // vertical gradient end
+           UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+           gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+           let image = UIGraphicsGetImageFromCurrentImageContext()
+           UIGraphicsEndImageContext()
+       self.navigationController!.navigationBar.setBackgroundImage(image, for: UIBarMetrics.default)
     }
 
     func setUpElements() {
         //Style the elements
         let backBarButtton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationItem.backBarButtonItem = backBarButtton
-            self.navigationController?.navigationBar.barTintColor = UIColor(named: "PurplePink")
         Utilities.styleFilledButton(loginButton)
-        Utilities.styleHollowButton(signUpButton)
+        Utilities.styleFilledButton(signUpButton)
     }
 
     @IBAction func loginButtonTapped(_ sender: UIButton) {
