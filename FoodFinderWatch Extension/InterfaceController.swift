7980c4d8f0a05b824fc88b944667807eb726c9a8
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-04-28 22:33:49 +0200

    Added Apple Watch feature

--- FoodFinderWatch Extension/InterfaceController.swift
+++ FoodFinderWatch Extension/InterfaceController.swift
@@ -8,24 +8,37 @@
 
 import WatchKit
 import Foundation
-
+import WatchConnectivity
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet weak var titleLabel: WKInterfaceLabel!
+    @IBOutlet weak var ingredientsLabel: WKInterfaceLabel!
+    let session = WCSession.default
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        session.delegate = self
+        session.activate()
     }
-    
+
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+}
+
+extension InterfaceController: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    
+    }
+
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        titleLabel.setText(message["title"] as? String)
+        ingredientsLabel.setText(message["ingredients"] as? String)
+        print("Recieved: \(message)")
+    }
 }
